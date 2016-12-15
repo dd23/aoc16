75912671b07aa7eab75ca79a5ffd322447c14bad
Daniel Demmler <daniel.demmler@gmail.com>
2016-12-15 23:47:44 +0100

    input, stub and comment for day 11

---
+++ 11/11.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+def part1():
+	for line in open("in11.txt"):
+		print(line)
+
+
+
+def part2():
+	pass
+
+
+
+def main():
+	part1()
+	#part2()
+
+
+if __name__ == '__main__':
+	main()
