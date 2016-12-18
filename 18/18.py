f30d0925714a931fcb7ff0b09a587b51615fd63d
Daniel Demmler <daniel.demmler@gmail.com>
2016-12-18 14:59:36 +0100

    improved day 18

--- 18/18.py
+++ 18/18.py
@@ -1,38 +1,43 @@
 # -*- coding: utf-8 -*-
 
-rows = []
-rounds = 40
+import re
 
-trap = ["^^.", ".^^", "^..", "..^"]
+rounds = 40
 
 def main():
 	inp = open("in18.txt").read()[:-1]
 	#inp = "..^^."
 
 	rlen = len(inp)
-	rows.append(inp)
+	rows = inp
 	res = inp.count(".")
 
 	for row in range(rounds-1):
 		newrow = ""
 		for idx in range(rlen):
 			if idx == 0:
-				check = "." + rows[row][:2]
+				check = "." != rows[1]
 			elif idx == rlen -1:
-				check = rows[row][idx-1:idx+1] + "."
+				check = rows[idx-1] != "."
 			else:
-				check = rows[row][idx-1:idx+2]
+				check = rows[idx-1] != rows[idx+1]
 
-			if check in trap:
+			if check:
 				newrow += "^"
 			else:
 				res +=1
 				newrow += "."
 
-		rows.append(newrow)
+		rows = newrow
 
 	print(res)
+
+def tt():
+	s = "^^."
+	rm = re.match(r"(.)\1(?!\1).|(.)((?!\2).)\3", s)
+	print(rm)
 
 
 if __name__ == '__main__':
 	main()
+	#tt()
