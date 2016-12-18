d96e5585a968324003a1b16845c45a64a2399497
Daniel Demmler <daniel.demmler@gmail.com>
2016-12-18 22:38:40 +0100

    day 18 improved

--- 18/18.py
+++ 18/18.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import re
-
 rounds = 40 #part 1
 #rounds = 400000 #part 2
 
 def main():
 	inp = open("in18.txt").read()[:-1]
-
-	rlen = len(inp) + 1
+	rlen = len(inp) - 1
 	row = inp
 	res = inp.count(".")
-	row = "." + row + "." # pad with "." to get rid of boundary checks
 
 	for _ in range(rounds - 1):
-		newrow = "."
-		for idx in range(1, rlen): # rlen is inputlen + 1
+		newrow = row[1]
+		for idx in range(1, rlen): # skip first and last idx
 			newrow += "^" if row[idx-1] != row[idx+1] else "."
-		res += newrow.count(".") - 1 # this is faster than +1 for every non-trap idx
-		row = newrow + "." # padding
+		row = newrow + row[-2]
+		res += row.count(".")# this is faster than +1 for every non-trap idx
 
 	print(res)
 
